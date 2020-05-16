a2e5e79eede42aa45c32d25491fab5d28c92bf99
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2020-05-16 01:31:01 +0100

    Add sound Also auto-format Lua code as the indentation was misleading in my editor.

--- init.lua
+++ init.lua
@@ -1,21 +1,21 @@
 local colors = {
-    {"red", "Red"},
-    {"orange", "Orange"},
-    {"violet", "Violet"},
-    {"green", "Green"},
+	{"red", "Red"},
+	{"orange", "Orange"},
+	{"violet", "Violet"},
+	{"green", "Green"},
 }
 
 
-for _,i in pairs(colors) do
+for _, i in pairs(colors) do
 	minetest.register_node("fireworks:"..i[1], {
 		description = i[2].." Fireworks",
 		tiles = {"firework_"..i[1]..".png"},
-		groups = {cracky=3, mesecon=2},
-                drawtype = "plantlike",
-                paramtype = "light",
-                selection_box = {
+		groups = {cracky = 3, mesecon = 2},
+		drawtype = "plantlike",
+		paramtype = "light",
+		selection_box = {
 			type = "fixed",
-			fixed = {-2 / 16, -0.5, -2 / 16, 2 / 16, 3 / 16, 2 / 16},
+			fixed = { - 2 / 16, - 0.5, - 2 / 16, 2 / 16, 3 / 16, 2 / 16},
 		},
 		on_punch = function(pos)
 			fireworks_activate(pos, i[1])
@@ -69,64 +69,69 @@ minetest.register_craft({
 })
 
 function fireworks_activate(pos, name)
-    minetest.remove_node(pos)
-    minetest.add_particlespawner({
+	minetest.remove_node(pos)
+	minetest.add_particlespawner({
 		amount = 1,
 		time = 0.001,
 		minpos = pos,
 		maxpos = pos,
 		minvel = 0,
 		maxvel = 0,
-		minacc = {x=0, y=13.2, z=0},
-		maxacc = {x=0, y=13.2, z=0},
+		minacc = {x = 0, y = 13.2, z = 0},
+		maxacc = {x = 0, y = 13.2, z = 0},
 		minexptime = 1.5,
 		maxexptime = 1.5,
 		minsize = 8,
 		maxsize = 8,
 		collisiondetection = false,
 		vertical = false,
-                glow = 5,
+		glow = 5,
 		texture = "firework_"..name..".png",
-    })
-    minetest.after(1.5, function()
-    local gravity = -8
-    pos.y = pos.y+15
-	minetest.add_particlespawner({
-		amount = 150,
-		time = 0.001,
-		minpos = pos,
-		maxpos = pos,
-		minvel = vector.new(-1,-1,-1),
-		maxvel = vector.new(1,1,1),
-		minacc = {x=0, y=-0.5, z=0},
-		maxacc = {x=0, y=-1, z=0},
-		minexptime = 2,
-		maxexptime = 2.5,
-		minsize = 2,
-		maxsize = 3,
-		collisiondetection = true,
-		vertical = false,
-                glow = 5,
-		texture = "firework_sparks_"..name..".png",
 	})
-        minetest.add_particlespawner({
-		amount = 100,
-		time = 0.001,
-		minpos = pos,
-		maxpos = pos,
-		minvel = vector.new(-1,-1,-1),
-		maxvel = vector.new(1,1,1),
-		minacc = {x=0, y=-0.5, z=0},
-		maxacc = {x=0, y=-1, z=0},
-		minexptime = 2,
-		maxexptime = 2.5,
-		minsize = 2,
-		maxsize = 3,
-		collisiondetection = true,
-		vertical = false,
-                glow = 5,
-		texture = "firework_sparks_blue.png",
-	})
-    end)
+	minetest.after(1.5, function()
+        minetest.sound_play("fireworks_explosion", {
+            pos = pos,
+            max_hear_distance = 100,
+            gain = 2.0
+        })
+        
+		local gravity = -8
+		pos.y = pos.y + 15
+		minetest.add_particlespawner({
+			amount = 150,
+			time = 0.001,
+			minpos = pos,
+			maxpos = pos,
+			minvel = vector.new(-1, - 1, - 1),
+			maxvel = vector.new(1, 1, 1),
+			minacc = {x = 0, y = -0.5, z = 0},
+			maxacc = {x = 0, y = -1, z = 0},
+			minexptime = 2,
+			maxexptime = 2.5,
+			minsize = 2,
+			maxsize = 3,
+			collisiondetection = true,
+			vertical = false,
+			glow = 5,
+			texture = "firework_sparks_"..name..".png",
+		})
+		minetest.add_particlespawner({
+			amount = 100,
+			time = 0.001,
+			minpos = pos,
+			maxpos = pos,
+			minvel = vector.new(-1, - 1, - 1),
+			maxvel = vector.new(1, 1, 1),
+			minacc = {x = 0, y = -0.5, z = 0},
+			maxacc = {x = 0, y = -1, z = 0},
+			minexptime = 2,
+			maxexptime = 2.5,
+			minsize = 2,
+			maxsize = 3,
+			collisiondetection = true,
+			vertical = false,
+			glow = 5,
+			texture = "firework_sparks_blue.png",
+		})
+	end)
 end
-
